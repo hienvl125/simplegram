3fde0db019717d928ce50bc8244e27cd09270926
hienvl125 <hienviluong125@gmail.com>
2023-01-21 14:03:50 +0700

    feat: Custom scrollbar

--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -5,8 +5,20 @@ interface MessageListProps {}
 
 const MessageList: FC<MessageListProps> = () => {
   return (
-    <div className="chat-bubble overflow-y-overlay grow overflow-y-auto">
+    <div className="chat-bubble green-scrollbar my-hover-overlay grow overflow-y-hidden">
       <div className="chat-bubble-wrapper mx-auto flex	min-h-full max-w-screen-md flex-col justify-end px-2 sm:px-0 sm:pr-16">
+        <Message isCurrentUser={true} />
+        <Message isCurrentUser={false} />
+        <Message isCurrentUser={true} />
+        <Message isCurrentUser={true} />
+        <Message isCurrentUser={false} />
+        <Message isCurrentUser={true} />
+        <Message isCurrentUser={true} />
+        <Message isCurrentUser={false} />
+        <Message isCurrentUser={true} />
+        <Message isCurrentUser={true} />
+        <Message isCurrentUser={false} />
+        <Message isCurrentUser={true} />
         <Message isCurrentUser={true} />
         <Message isCurrentUser={false} />
         <Message isCurrentUser={true} />
